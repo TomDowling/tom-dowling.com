79fe442065f284a37badea823ae3188e56c1ea3b
Tom Dowling <tom@digital-space.io>
2023-07-14 23:44:26 +0100

    feat: setting up the project at the start with defaults and standards I want to follow

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#3C9AB6',
+        primary2: '#E40037',
+        secondary: '#1c104f',
+      },
+      aspectRatio: {
+        '4/3': '4 / 3',
       },
     },
   },
